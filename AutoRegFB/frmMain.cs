c115087197cf0aee5d9697e5209777cdd8c6fa88
thuylnvnext <thuyln@vnext.vn>
2015-07-31 23:30:55 +0700

    Change time wait to next

--- AutoRegFB/frmMain.cs
+++ AutoRegFB/frmMain.cs
@@ -1515,10 +1515,9 @@ namespace AutoRegFB
                         if (acceptPlay != null)
                         {
                             acceptPlay.Click();
-                            //TIMER_ACCEPT.Interval = 2000;
-                            //TIMER_ACCEPT.Enabled = true;
-                            //TIMER_ACCEPT.Tick += new System.EventHandler(this.timer_Accept_Tick);
-                            geckoWebBrowser.Reload(GeckoLoadFlags.IsRefresh);
+                            TIMER_ACCEPT.Interval = 1000;
+                            TIMER_ACCEPT.Enabled = true;
+                            TIMER_ACCEPT.Tick += new System.EventHandler(this.timer_Accept_Tick);
                             return;
                         }
 
