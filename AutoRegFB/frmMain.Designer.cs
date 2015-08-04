0c6870a5ae18f57dea06367f72ea54aa031aa566
thuylnvnext <thuyln@vnext.vn>
2015-08-04 22:37:20 +0700

    Add auto invite

--- AutoRegFB/frmMain.Designer.cs
+++ AutoRegFB/frmMain.Designer.cs
@@ -41,6 +41,7 @@
             this.btnCleanMsg = new System.Windows.Forms.Button();
             this.btnGetPinCode = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnInvite = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // geckoWebBrowser
@@ -64,7 +65,7 @@
             // 
             // btnLoginFB
             // 
-            this.btnLoginFB.Location = new System.Drawing.Point(575, 31);
+            this.btnLoginFB.Location = new System.Drawing.Point(575, 60);
             this.btnLoginFB.Name = "btnLoginFB";
             this.btnLoginFB.Size = new System.Drawing.Size(99, 23);
             this.btnLoginFB.TabIndex = 4;
@@ -163,11 +164,22 @@
             this.btnLogOut.UseVisualStyleBackColor = true;
             this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
+            // btnInvite
+            // 
+            this.btnInvite.Location = new System.Drawing.Point(575, 30);
+            this.btnInvite.Name = "btnInvite";
+            this.btnInvite.Size = new System.Drawing.Size(99, 23);
+            this.btnInvite.TabIndex = 15;
+            this.btnInvite.Text = "Invite";
+            this.btnInvite.UseVisualStyleBackColor = true;
+            this.btnInvite.Click += new System.EventHandler(this.btnInvite_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(678, 486);
+            this.Controls.Add(this.btnInvite);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnGetPinCode);
             this.Controls.Add(this.btnCleanMsg);
@@ -207,6 +219,7 @@
         private System.Windows.Forms.Button btnCleanMsg;
         private System.Windows.Forms.Button btnGetPinCode;
         private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnInvite;
     }
 }
 
